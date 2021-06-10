abc95707210f4d68bcd0df3e0cfb53f11d659c7f
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-10 15:27:16 +0200

    Fichier PHP

---
+++ Exo 5 partie 4 PHP.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 5 : Les boucles</title>
+</head>
+<body>
+    <h1>Exo 5 : Les boucles</h1>
+    <h2>Exo 5</h2>
+    <p>
+    En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.
+    </p>
+    <p>
+        <?php
+        for($i = 1; $i < 15; $i++) {
+            echo'on y arrive presque'.'<br>';
+        }
+        ?>
+    </p>
+</body>
+</html>
